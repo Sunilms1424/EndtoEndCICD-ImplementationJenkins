5286f5d9b51f7b7ac6e097f8b9d7a3822517cc87
Sunilms1424 <132062594+Sunilms1424@users.noreply.github.com>
2024-09-05 18:16:38 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 #pull the docker image from docker hub
-docker pull sunhar0724/jenins_cicd_project:1
+docker pull sunhar0724/jenins_cicd_project:2
 
 #run the docker images as a container
 
-docker run -d -p 8000:8000 sunhar0724/jenins_cicd_project:1
+docker run -d -p 8000:8000 sunhar0724/jenins_cicd_project:2
